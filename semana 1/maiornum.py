8fcaf619ae9756974dca240e717a6075c629caf4
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2023-04-27 15:20:14 -0300

    quinta

---
+++ semana 1/maiornum.py
@@ -0,0 +1,4 @@
+numeros = []
+while len(numeros) < 5:
+    numeros.append(input('Digite um numero: '))
+print('O maior numero entre os 5 digitados é: ', max(numeros))
---
+++ semana 1/positivoounegativo.py
@@ -0,0 +1,5 @@
+valor = int(input('Informe um valor: '))
+if valor < 0:
+  print('O valor é negativo')
+else:
+  print('O valor é positivo')
