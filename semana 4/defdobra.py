26b7c00f9b2ca2b17ed944ecd401c608ed70737d
Thaís Melquíades <85419606+tmelquiades@users.noreply.github.com>
2023-05-17 16:55:07 -0300

    wed

---
+++ semana 4/defdobra.py
@@ -0,0 +1,8 @@
+def dobra(lst):
+    pos=0
+    while pos<len(lst):
+        lst[pos]*=2
+
+valores=[7,2,5,0,4]
+dobra(valores)
+print(valores)
---
+++ semana 4/somafunc.py
@@ -0,0 +1,6 @@
+def soma():
+    a = int(input('Digite um numero: '))
+    b = int(input('Digite outro numero: '))
+    print(a+b)
+
+soma()
